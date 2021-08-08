01242f517fe62f7af6f3869c69f0c750c7020ab7
Burak <eselik.burak@gmail.com>
2021-08-08 13:45:52 +0300

    some functions added

--- hangman.py
+++ hangman.py
@@ -1,21 +1,101 @@
 # a console game
-# date: aug 7 2021
+# date: aug 7,8 2021
 from random import choice
 
+word_i = str()
 
-word_list = ['dance', 'python', 'pygame', 'tkinter', 'basketball']
+def fun_pattern(word:str, *args):
+	global word_i
+	if len(args) == 2:
+		guess, index_nums = args
+		word_i = edit_word_i(word_i, guess, index_nums)
+	elif len(args) == 1:
+		...
+	else:
+		word_i = "__ "*len(word)
+		
+
+	pattern = [' |-------', ' |   |', ' |    ', r' |      ', ' |    ', r' |      ', ' |', f'---\t\t word: {word_i}']
+	for i in pattern:
+		print(i)
+
+def edit_word_i(word_i:str, guess:str, index_nums:list) -> str:
+	word_a = word_i.split()
+
+	for j in index_nums:
+		word_a[j] = guess
+
+	word_a = ' '.join(word_a)
+
+	return word_a
 
 def get_a_word() -> str:
-	...
+	word_list = ['basketball']
 	return choice(word_list)
 
-def main() -> None:
-	word = get_a_word()
-	return word
 
+def get_guess() -> str:	
+	while 1:
+		word_or_letter = input('Guess a letter (1)\nGuess the word (2) : ')
+		if word_or_letter == '1':
+			while 1:
+				guess = input('Type a letter :')
+				if len(guess) != 1:
+					print('Please just type one letter!') 
+					continue
+				else: break
+			break
+		elif word_or_letter == '2':
+			guess = input('Type the word :')
+			break
+		else:
+			print('Do Not Type Other Things!(or q)')
+			continue
+	return guess
+
+
+def get_index_nums(letter, word:str ) -> list:
+	'''
+	:aim     : finds index numbers of given letter in the word:
+	:param   : letter: required
+	:param   : word  : required
+	:returns : a list ?:
+	'''
+	index_nums = list()
+
+	while 1:
+		try:
+			index = word.find(letter, index_nums[-1] + 1)
+			if index == -1:
+				break
+			index_nums.append(index)
+			#TODO HOW TO QUIT THIS LOOP?(handled)
+		except IndexError:
+			index_nums.append(word.find(letter, 0))
+	
+	return index_nums
+
+
+#game play
+def main() -> None:
+	word = get_a_word().upper()
+	fun_pattern(word)
+
+	while 1:
+		
+		guess = get_guess().upper().strip()
+		if guess == word:
+			print('YOU WON!')
+			break
+		elif guess == 'Q': exit()
+		elif guess in word and len(guess) == 1:
+			index_nums = get_index_nums(guess, word)
+			fun_pattern(word, guess, index_nums)
+		else:
+			fun_pattern(word, False)
 
 
 if __name__ == '__main__':
 	print('Welcome to hangman game!')
 	print('Guess the word!')
-	print(main())
+	main()
